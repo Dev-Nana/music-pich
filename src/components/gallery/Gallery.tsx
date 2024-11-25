f2d817fb91b27020513e8495477cb1bd32943639
Nana <evylaveras@gmail.com>
2024-11-25 01:10:15 -0300

    refactor: revisão de Gallery

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,11 +1,11 @@
-import { Section } from "../section/Section";
+import { Section } from "../section/section";
 import { Filter } from "../filter/Filter";
 
 export const Gallery = () => {
   return (
     <div className="bg-gray-950">
       <Section />
-      <Filter />  
+      <Filter />
     </div>
   );
 }
