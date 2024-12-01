9165f7c6899a6c031c838901b14ee64c776b149d
Nana <evylaveras@gmail.com>
2024-11-30 23:32:48 -0300

    fix: corrija estrutura do componente Button

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Button = () => {
   return (
       <button className='border border-purple-150 rounded-lg px-5 py-2 text-purple-150 font-medium mt-3 text-xs'>
